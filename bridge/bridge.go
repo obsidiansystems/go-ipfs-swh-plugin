47117b18dca32a81e4936d93f3a694574c1e13f5
John Ericson <John.Ericson@Obsidian.Systems>
2022-07-19 23:15:21 -0400

    Split out config parser from callback + upcast

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -293,35 +293,39 @@ func (cfg *bridgeDatastoreConfig) Create(string) (repo.Datastore, error) {
 	return BridgeDs{cfg}, nil
 }
 
+func ParseConfig(params map[string]interface{}) (*bridgeDatastoreConfig, error) {
+	base_url_v, ok := params["base-url"]
+	base_url_s := "https://archive.softwareheritage.org"
+	if ok {
+		base_url_s, ok = base_url_v.(string)
+		if !ok {
+			return nil, fmt.Errorf("base-url %q is not a string", base_url_v)
+		}
+	}
+
+	base_url, err := url.Parse(base_url_s)
+	if err != nil {
+		return nil, err
+	}
+
+	var auth_token *string
+	auth_token_v, ok := params["auth-token"]
+	if ok {
+		auth_token_s, ok := auth_token_v.(string)
+		if !ok {
+			return nil, fmt.Errorf("auth-token %q is not a string", auth_token_v)
+		}
+		auth_token = &auth_token_s
+	}
+
+	return &bridgeDatastoreConfig{
+		base_url,
+		auth_token,
+	}, nil
+}
+
 func (*BridgePlugin) DatastoreConfigParser() fsrepo.ConfigFromMap {
 	return func(params map[string]interface{}) (fsrepo.DatastoreConfig, error) {
-		base_url_v, ok := params["base-url"]
-		base_url_s := "https://archive.softwareheritage.org"
-		if ok {
-			base_url_s, ok = base_url_v.(string)
-			if !ok {
-				return nil, fmt.Errorf("base-url %q is not a string", base_url_v)
-			}
-		}
-
-		base_url, err := url.Parse(base_url_s)
-		if err != nil {
-			return nil, err
-		}
-
-		var auth_token *string
-		auth_token_v, ok := params["auth-token"]
-		if ok {
-			auth_token_s, ok := auth_token_v.(string)
-			if !ok {
-				return nil, fmt.Errorf("auth-token %q is not a string", auth_token_v)
-			}
-			auth_token = &auth_token_s
-		}
-
-		return &bridgeDatastoreConfig{
-			base_url,
-			auth_token,
-		}, nil
+		return ParseConfig(params)
 	}
 }
